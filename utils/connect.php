8a4f5f31b1136642d4859d7aeac6bb68a31cebe5
Matthieu Vidal <matthieuvdl93@gmail.com>
2020-06-17 18:49:34 +0200

    Connection handler

---
+++ utils/connect.php
@@ -0,0 +1,25 @@
+<?php
+
+// Errors
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+
+// DB
+if($_SERVER['HTTP_HOST'] === 'localhost') // Local Server
+{
+    define('DB_HOST', 'localhost');
+    define('DB_PORT', '3306');
+    define('DB_USER', 'dbuser');
+    define('DB_PASS', 'password');
+}
+else if($_SERVER['HTTP_HOST'] === 'host.fr') // Remote Server
+{
+    define('DB_HOST', 'host.fr');
+    define('DB_PORT', '3306');
+    define('DB_USER', 'dbuser');
+    define('DB_PASS', 'password');
+}
+define('DB_NAME', 'dbname');
+
+$pdo = new PDO('mysql:host='.DB_HOST.';port='.DB_PORT.';dbname='.DB_NAME, DB_USER, DB_PASS);
+$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
